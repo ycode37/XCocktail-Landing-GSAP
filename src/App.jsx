27add50b24a1bd3bf4232b1ff48aa034fc9d7458
Yash Bothra <yash.yb.bothra@gmail.com>
2026-04-09 19:12:02 +0530

    Video Playback

--- src/App.jsx
+++ src/App.jsx
@@ -121,7 +121,6 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      <div className="h-dvh"></div>
     </main>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,39 @@
-import React from "react";
-import { navLinks } from "../../Constants";
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { navLinks } from "../../Constants";
 
 const Navbar = () => {
-  const navbar = () => {
-    useGSAP(() => {
-      const navtween = gsap.timeline({
-        scrollTrigger: {
-          trigger: "nav",
-          start: "bottom top",
-        },
-      });
-      navtween.fromTo(
-        "nav",
-        { backgroundColor: "transparent" },
-        {
-          backgroundColor: "#00000050",
-          backgroundFilter: "blur(10px)",
-          duration: 1,
-          ease: "power1.inOut",
-        },
-      );
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
     });
-  };
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      },
+    );
+  });
+
   return (
     <nav>
       <div>
-        <a href="home" className="flex items-center gap-2">
-          <p>Hollow Purple</p>
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" alt="logo" />
+          <p>Velvet Pour</p>
         </a>
+
         <ul>
           {navLinks.map((link) => (
-            <li key={link.title}>
+            <li key={link.id}>
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
@@ -41,5 +42,4 @@ const Navbar = () => {
     </nav>
   );
 };
-
 export default Navbar;
